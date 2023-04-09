78d7bf45292d87dbd4e9f1cf5fa2e83959c1a44f
Daniel27110 <d.vargasu@uniandes.edu.co>
2023-04-08 22:59:11 -0500

    Taller DONE

--- main.ts
+++ main.ts
@@ -3,8 +3,9 @@ import { Serie } from "./serie.js";
 import { series } from "./data.js";
 
 
-let seriesTbody: HTMLTableSectionElement = document.getElementById("series") as HTMLTableSectionElement;
+let seriesTbody: HTMLTableSectionElement = document.getElementById("seriesTable") as HTMLTableSectionElement;
 let averageSeasonsSpan: HTMLSpanElement = document.getElementById("averageSeasons") as HTMLSpanElement;
+let seriesCard: HTMLDivElement = document.getElementById("seriesCard") as HTMLDivElement;
 
 renderSeriesInTable(series);
 averageSeasons(series);
@@ -19,11 +20,15 @@ function renderSeriesInTable(series: Serie[]): void {
             <td>${serie.channel}</td>
             <td>${serie.seasons}</td>
         `;
+        // makes the row clickable, when clicked it will call the function showSerieCard
+        tr.addEventListener("click", () => {
+            showSerieCard(serie);
+        });
         seriesTbody.appendChild(tr);
     });
 }
 
-// creates a function that calculates the average of the seasons of the series
+// Function that calculates the average of the seasons of the series
 function averageSeasons(series: Serie[]): void {
     let totalSeasons: number = 0;
     series.forEach(serie => {
@@ -31,4 +36,18 @@ function averageSeasons(series: Serie[]): void {
     });
     averageSeasonsSpan.innerText = "Seasons average: " + (totalSeasons / series.length).toFixed(0);
 
+}
+
+// Function that shows a bootstrap card with the image, title, a description of the serie and a link to the series
+function showSerieCard(serie: Serie): void {
+    seriesCard.innerHTML = `
+        <div class="card">
+            <img src="${serie.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${serie.title}</h5>
+                <p class="card-text">${serie.description}</p>
+                <a href="${serie.url}" class="btn btn-primary">Go to serie</a>
+            </div>
+        </div>
+    `;
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import { series } from "./data.js";
-var seriesTbody = document.getElementById("series");
+var seriesTbody = document.getElementById("seriesTable");
 var averageSeasonsSpan = document.getElementById("averageSeasons");
+var seriesCard = document.getElementById("seriesCard");
 renderSeriesInTable(series);
 averageSeasons(series);
 function renderSeriesInTable(series) {
@@ -8,14 +9,22 @@ function renderSeriesInTable(series) {
         var tr = document.createElement("tr");
         tr.classList.add("table-secondary");
         tr.innerHTML = "\n            <th scope=\"row\">".concat(serie.id, "</th>\n            <td><a href=\"").concat(serie.url, "\">").concat(serie.title, "</a></td>\n            <td>").concat(serie.channel, "</td>\n            <td>").concat(serie.seasons, "</td>\n        ");
+        // makes the row clickable, when clicked it will call the function showSerieCard
+        tr.addEventListener("click", function () {
+            showSerieCard(serie);
+        });
         seriesTbody.appendChild(tr);
     });
 }
-// creates a function that calculates the average of the seasons of the series
+// Function that calculates the average of the seasons of the series
 function averageSeasons(series) {
     var totalSeasons = 0;
     series.forEach(function (serie) {
         totalSeasons += serie.seasons;
     });
     averageSeasonsSpan.innerText = "Seasons average: " + (totalSeasons / series.length).toFixed(0);
+}
+// Function that shows a bootstrap card with the image, title, a description of the serie and a link to the series
+function showSerieCard(serie) {
+    seriesCard.innerHTML = "\n        <div class=\"card\">\n            <img src=\"".concat(serie.image, "\" class=\"card-img-top\" alt=\"...\">\n            <div class=\"card-body\">\n                <h5 class=\"card-title\">").concat(serie.title, "</h5>\n                <p class=\"card-text\">").concat(serie.description, "</p>\n                <a href=\"").concat(serie.url, "\" class=\"btn btn-primary\">Go to serie</a>\n            </div>\n        </div>\n    ");
 }
